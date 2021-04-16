cc4fd4d2b6c4bdd06a7363080dc1ea11e34020d7
Ekoh123 <janet.ekoh@azubiafrica.org>
2021-04-16 23:52:47 +0000

    added email field

--- reset.php
+++ reset.php
@@ -34,8 +34,8 @@ if(count($_SESSION) != 0){
         </div>
         <div class="form">
             <form method="POST">                                    
-                <input type="password" id="pword" name="pword" placeholder="PASSWORD"><br><br>
                 <input type="email" id="email" name="email" placeholder="test@email.com"><br><br>
+                <input type="password" id="pword" name="pword" placeholder="PASSWORD"><br><br>
                 <input type="submit" name="submit" value="submit">                
             </form>
         </div>
@@ -44,16 +44,15 @@ if(count($_SESSION) != 0){
         if(isset($_POST['submit'])){
 
             $newpassword= $_POST['pword'];
+            $emailinput=$_POST['email'];
             $message='';
-
-            $userfile = 'userdata.txt';
-            $openfile = file('userdata.txt');
-            $fileopena = fopen($userfile,"a");
-            //$fileread = fread($fileopenr, filesize($userfile));
-
+           
+            //$fileopena = fopen($userfile,"a");
+           
             //session area
             $firstname= $_SESSION['fname'] ;
             $lastname= $_SESSION['lname'] ;
+            $email=$_SESSION['email'];
         
             if(empty($newpassword)) {
                   $message .= "<li>Password cannot be empty</li>";
@@ -62,20 +61,39 @@ if(count($_SESSION) != 0){
                 if(!empty($message)) {
                     echo "<script type='text/javascript'>alert('$message');</script>";
                 }else{
-                    $success = false;
-                    foreach ($openfile as $user) {
-                        $user_details = explode(',', $user);
-                        
-                        if (trim($user_details[0]) == $firstname && trim($user_details[1]) == $lastname) {
-                            $success = true;
-                            echo $success;
-                            $search = $user_details[2];
-                            $str = str_replace ( $search , $newpassword , $openfile );
-                            fwrite($fileopena, $str);
-                            fclose($fileopena);
-                            break;
-                        }
-                    }
+                    $userfile = 'userdata.txt';
+                    $openfile = file('userdata.txt');
+                    $fileopenr = fopen($userfile, 'r');
+                    $fileread = fread($fileopenr, filesize($userfile));
+                    $searchposition = strpos( $fileread, $email);     
+                               
+
+                    if ($searchposition === false) {
+                        echo "<script type='text/javascript'>alert('Please sign up for a new account');</script>";         
+                   }else{
+
+                       foreach ($openfile as $user) {
+                           $user_details = explode(',', $user);
+                           
+                           if ($email === $emailinput) {
+                            if($user_details[2]===$email){
+
+                                $search = $user_details[3];
+                                $str = str_replace ( $search , $newpassword , $openfile );
+                                $file_details=implode(',',$str);
+                                $fileopena = fopen($userfile,"a");
+                                fwrite($fileopena, $file_details);
+                                fclose($fileopena);
+                            }
+                           }else{
+                               $success=false;
+                               echo $success;
+                           }
+                       }
+                   }
+                   fclose($fileopenr);
+                   header("Location: landing.php");               
+                   exit;
                 }
         }
 
